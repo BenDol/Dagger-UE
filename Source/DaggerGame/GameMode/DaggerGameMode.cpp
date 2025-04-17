a0eaf7d353aab6e49383b7fcc29f6e135d079f56
Ben Dol <dolb90@gmail.com>
2025-04-17 10:29:49 -0400

    WIP Compiling now

---
+++ Source/DaggerGame/GameMode/DaggerGameMode.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DaggerGameMode.h"
+
+#include "Character/DaggerCharacter.h"
+#include "Player/DaggerPlayerController.h"
+#include "Player/DaggerPlayerState.h"
+
+ADaggerGameMode::ADaggerGameMode(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    PlayerControllerClass = ADaggerPlayerController::StaticClass();
+    ReplaySpectatorPlayerControllerClass = ADaggerReplayPlayerController::StaticClass();
+    PlayerStateClass = ADaggerPlayerState::StaticClass();
+    DefaultPawnClass = ADaggerCharacter::StaticClass();
+}
---
+++ Source/DaggerGame/GameMode/DaggerGameMode.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameMode/DaggerGameModeBase.h"
+#include "DaggerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAGGERGAME_API ADaggerGameMode : public ADaggerGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    ADaggerGameMode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+};
