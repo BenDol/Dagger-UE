a667ce03aff705a2049d42a049e539bbaa856fc6
Ben Dol <dolb90@gmail.com>
2025-04-22 12:01:55 -0400

    Add DaggerModuleRulesBase

--- Source/DaggerGame/DaggerGame.Build.cs
+++ Source/DaggerGame/DaggerGame.Build.cs
@@ -1,117 +1,12 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using DaggerBuildTool;
 using UnrealBuildTool;
 
-public class DaggerGame : ModuleRules
+public class DaggerGame : DaggerGameModuleRulesBase
 {
-    public DaggerGame(ReadOnlyTargetRules Target) : base(Target)
+    public DaggerGame(ReadOnlyTargetRules Target) : base(Target, "DaggerGame")
     {
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
-		PublicIncludePaths.AddRange(new string[] {
-			"DaggerGame"
-        }); 
-
-		PrivateIncludePaths.AddRange(new string[] {
-            
-		});
-
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"Core",
-			"CoreOnline",
-			"CoreUObject",
-			"ApplicationCore",
-			"Engine",
-			"PhysicsCore",
-			"GameplayTags",
-			"GameplayTasks",
-			"GameplayAbilities",
-			"AIModule",
-			"ModularGameplay",
-			"ModularGameplayActors",
-			"DataRegistry",
-			"ReplicationGraph",
-			"GameFeatures",
-			"SignificanceManager",
-			"Hotfix",
-			"CommonLoadingScreen",
-			"Niagara",
-			"AsyncMixin",
-			"ControlFlows",
-			"PropertyPath",
-            "GameSettings",
-            "GameSettingsUI",
-            "DaggerBuildTools",
-            "DaggerCore",
-            "DaggerCharacter",
-            "DaggerCharacterUI",
-            "DaggerFeedback",
-            "DaggerPhysics",
-            "DaggerHotfix",
-            "DaggerSettings",
-            "DaggerSystem",
-            "DaggerActions",
-            "DaggerPerformance",
-            "DaggerCamera",
-            "DaggerAudio",
-            "DaggerDefinitions",
-            "DaggerInput",
-            "DaggerUI",
-		});
-
-		PrivateDependencyModuleNames.AddRange(new string[] {
-			"InputCore",
-			"Slate",
-			"SlateCore",
-			"RenderCore",
-			"DeveloperSettings",
-			"EnhancedInput",
-			"NetCore",
-			"RHI",
-			"Projects",
-			"Gauntlet",
-			"UMG",
-			"CommonUI",
-			"CommonInput",
-            "GameSettings",
-            "GameSettingsUI",
-			"CommonGame",
-			"CommonUser",
-			"GameSubtitles",
-			"GameplayMessageRuntime",
-			"AudioMixer",
-			"NetworkReplayStreaming",
-			"UIExtension",
-			"ClientPilot",
-			"AudioModulation",
-			"EngineSettings",
-            "DTLSHandlerComponent",
-			"Json",
-		});
-
-		DynamicallyLoadedModuleNames.AddRange(new string[] {
-            
-		});
-
-		// Generate compile errors if using DrawDebug functions in test/shipping builds.
-		PublicDefinitions.Add("SHIPPING_DRAW_DEBUG_ERROR=1");
-
-		// Basic setup for External RPC Framework.
-		// Functionality within framework will be stripped in shipping to remove vulnerabilities.
-		PrivateDependencyModuleNames.Add("ExternalRpcRegistry");
-		PrivateDependencyModuleNames.Add("HTTPServer"); // Dependency for ExternalRpcRegistry
-		if (Target.Configuration == UnrealTargetConfiguration.Shipping)
-		{
-			PublicDefinitions.Add("WITH_RPC_REGISTRY=0");
-			PublicDefinitions.Add("WITH_HTTPSERVER_LISTENERS=0");
-		}
-		else
-		{
-			PublicDefinitions.Add("WITH_RPC_REGISTRY=1");
-			PublicDefinitions.Add("WITH_HTTPSERVER_LISTENERS=1");
-		}
-
-		SetupGameplayDebuggerSupport(Target);
-		SetupIrisSupport(Target);
+	    //
     }
 }
--- Source/DaggerGameEditor/DaggerGameEditor.Build.cs
+++ Source/DaggerGameEditor/DaggerGameEditor.Build.cs
@@ -1,90 +1,11 @@
-﻿using UnrealBuildTool;
+﻿using DaggerBuildTool;
+using UnrealBuildTool;
 
-public class DaggerGameEditor : ModuleRules
+public class DaggerGameEditor : DaggerEditorModuleRulesBase
 {
-    public DaggerGameEditor(ReadOnlyTargetRules Target) : base(Target)
+    public DaggerGameEditor(ReadOnlyTargetRules Target) 
+        : base(Target, "DaggerGame", "DaggerGameEditor")
     {
-        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
-        PublicIncludePaths.AddRange(new string[] {
-            "DaggerGameEditor"
-        });
-
-        PrivateIncludePaths.AddRange(new string[] {
-            
-        });
-
-        PublicDependencyModuleNames.AddRange(new string[] {
-            "Core",
-            "CoreUObject",
-            "Engine",
-            "EditorFramework",
-            "UnrealEd",
-            "PhysicsCore",
-            "GameplayTagsEditor",
-            "GameplayTasksEditor",
-            "GameplayAbilities",
-            "GameplayAbilitiesEditor",
-            "StudioTelemetry",
-            "GameSettings",
-            "GameSettingsUI",
-            "DaggerEditor",
-            "DaggerCore",
-            "DaggerCharacter",
-            "DaggerCharacterUI",
-            "DaggerFeedback",
-            "DaggerPhysics",
-            "DaggerHotfix",
-            "DaggerSettings",
-            "DaggerSystem",
-            "DaggerActions",
-            "DaggerPerformance",
-            "DaggerCamera",
-            "DaggerAudio",
-            "DaggerDefinitions",
-            "DaggerInput",
-            "DaggerUI",
-            "DaggerGame",
-        });
-
-        PrivateDependencyModuleNames.AddRange(new string[] {
-            "InputCore",
-            "Slate",
-            "SlateCore",
-            "ToolMenus",
-            "EditorStyle",
-            "DataValidation",
-            "MessageLog",
-            "Projects",
-            "DeveloperToolSettings",
-            "CollectionManager",
-            "SourceControl",
-            "Chaos",
-            "GameSettings",
-            "GameSettingsUI",
-            "DaggerGame",
-            "DaggerEditor",
-        });
-
-        DynamicallyLoadedModuleNames.AddRange(new string[] {
-            
-        });
-        
-        // Basic setup for External RPC Framework.
-        // Functionality within framework will be stripped in shipping to remove vulnerabilities.
-        PrivateDependencyModuleNames.Add("ExternalRpcRegistry");
-        if (Target.Configuration == UnrealTargetConfiguration.Shipping)
-        {
-            PublicDefinitions.Add("WITH_RPC_REGISTRY=0");
-            PublicDefinitions.Add("WITH_HTTPSERVER_LISTENERS=0");
-        }
-        else
-        {
-            PrivateDependencyModuleNames.Add("HTTPServer");
-            PublicDefinitions.Add("WITH_RPC_REGISTRY=1");
-            PublicDefinitions.Add("WITH_HTTPSERVER_LISTENERS=1");
-        }
-        // Generate compile errors if using DrawDebug functions in test/shipping builds.
-        PublicDefinitions.Add("SHIPPING_DRAW_DEBUG_ERROR=1");
+        //
     }
 }
